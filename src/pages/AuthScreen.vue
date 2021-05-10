57e33eaa0815541ba8355b54b12e4b853f31cb83
Itumeleng Tsoela <thiotsoela@gmail.com>
2021-05-10 05:52:28 +0200

    login

--- src/pages/AuthScreen.vue
+++ src/pages/AuthScreen.vue
@@ -1,13 +1,13 @@
 <template>
     <q-page>
-        <main class="row">
-            <div class="col-12 col-md-6 left q-pa-xl bg-primary">   
+        <main>
+            <div class="q-pa-xl bg-primary">   
                 <i class="fab middle fa-twitter"></i>
             </div>
-            <div class="col-12 col-md-6 bg-white right q-pa-xl"> 
+            <div class="bg-white q-pa-xl"> 
                 <div class="second-inside">
                     <i class="fab fa-twitter"></i>
-                    <div class="text text-weight-bolder"> See whats happening in<br> the world right now <span class="text-weight-bold">Join Twitter Today.</span></div>
+                    <div class="text text-weight-bolder gt-sm"> See whats happening in<br> the world right now <span class="text-weight-bold">Join Twitter Today.</span></div>
                     <q-btn style="width: 80%" :to="{name: 'Register'}" class="q-py-sm q-my-md" unelevated rounded color="primary" label="Sign up" />
                     <q-btn style="width: 80%" :to="{name: 'Login'}" class="q-py-sm" outline rounded color="primary" label="Log in" />
                 </div>
@@ -24,31 +24,39 @@
 
 <style lang="scss" scoped>
 
-    @mixin centerItems() {
+    @mixin centerItems {
         display: flex;
         align-items: center;
         justify-content: center;
 
-        @media (min-width: 1023px) {
-            height: 100vh;
-        }
-        @media (max-width: 1022px) {
-            height: 48vh;
-        }
+        // @media (min-width: 1023px) {
+        //     height: 100vh;
+        // }
+        // @media (max-width: 1022px) {
+        //     height: 48vh;
+        // }
         
     }
 
     main {
-        .left {
-            @include centerItems();
+        display: grid;
+        grid-template-columns: 1fr;
+        width: 100%;
+        
+        div:first-child {
+             @include centerItems;
+             width: 100%;
+            
             .fa-twitter {
                 color: white;
                 font-size: 10rem;
             }
         }
 
-        .right {
+        div:last-child {
             @include centerItems();
+            flex-direction: column;
+            width: 100%;
             .fa-twitter {
                 font-size: 2.5rem;
                 color: #027BE3;
@@ -66,6 +74,11 @@
                 }
             }
         }
+    }
 
+    @media (min-width: 1023px) {
+        main {
+            display: flex;
+        }
     }
 </style>
